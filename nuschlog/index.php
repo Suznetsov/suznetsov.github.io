d36deb0c68457aee86a248a6625a9359a5209e76
Suznetsov <jew2shroom@proton.me>
2025-09-08 17:50:25 -0500

    Create index.php

---
+++ nuschlog/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include 'variables.php' ?>
+    <title><?php echo "$domainName"; ?></title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+  </head>
+  <body>
+    <div class="box">
+      <img src="/images/mascot.png" alt="A mascot for <?php echo "$domainName"; ?>" id="mascot">
+      <h1>soyjaks.blog</h1>
+      <h2><a href="forums.html" id="rowan">Forums</a></h2>
+      <p>
+        <!-- Why does your site exist? What is its purpose? -->
+        soyjaks.blog was made as a successor to soyjak.blog, a 
+        site related to the soyjak meme and blogging. This site
+        is a forum designated for the imageboard subculture, more specifically,
+        that of Soyjak Party's. Read the <a href="rules.html">rules</a> before
+        posting.
+      </p>
+    </div>
+    <br>
+    <div class="box">
+      <nav id="footer"></nav>
+    </div>
+    <script src="script.js"></script>
+  </body>
+</html>
