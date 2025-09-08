06bc38ff84754a52488c0098dc94a3dfd6691198
Suznetsov <jew2shroom@proton.me>
2025-09-08 18:30:06 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("footer").innerHTML = `[<a href="index.html">index</a> /
+<a href="rules.html">rules</a> /
+<a href="forums.html">forums</a>]
+[<a href="http://www.soyjak.st/">party</a> /
+<a href="http://soybooru.com">booru</a> /
+<a href="http://soyjakwiki.org">wiki</a>]`
